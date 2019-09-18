3efe79ea14c2be09ce5a2f22ee238d86d16a4703
audeta35 <audeta35@gmail.com>
2019-09-18 20:42:47 +0700

    create first page with bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+//use path module
+const path = require('path');
+//use express module
 const express = require('express');
+//use hbs view engine
+const hbs = require('hbs');
 const app = express();
 
+//set dynamic views file
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine', 'hbs');
+//set public folder as static folder for static file
+app.use(express.static('public'));
 //route untuk halaman home
 app.get('/',(req, res) => {
-  res.send('Welcome To Express');
+  //render file index.hbs
+  res.render('index');
+});
+
+app.get('/test',(req, res) =>{
+
+  res.send('Test page');
 });
 
 //route untuk halaman about
