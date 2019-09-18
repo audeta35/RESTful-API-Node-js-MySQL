a00cfb57295a2364b591f8b721d9b128112b06b9
audeta35 <audeta35@gmail.com>
2019-09-18 21:19:31 +0700

    create firt form with node js

--- app.js
+++ app.js
@@ -4,19 +4,40 @@ const path = require('path');
 const express = require('express');
 //use hbs view engine
 const hbs = require('hbs');
+//use bodyParser middleware
+const bodyParser = require('body-parser');
+
 const app = express();
 
 //set views file
 app.set('views',path.join(__dirname,'views'));
+
 //set view engine
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 //set public folder as static folder for static file
 app.use(express.static('public'));
-//route untuk halaman home
+
+//route untuk halaman utama
 app.get('/',(req, res) => {
   //render file index.hbs
   res.render('index',{
     name : "Audeta Sandy"
+  });
+});
+
+//route form.hbs
+app.get('/post', (req, res) => {
+  res.render('form');
+});
+
+//route post form.hbs
+app.post('/post', (req, res) => {
+  res.render('index', {
+
+    name : req.body.username
   });
 });
 
